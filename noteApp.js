6433f3c9122b034faeb93055e34801050772938f
Ankan1998 <ankanroni3@1998>
2021-11-25 16:56:03 +0530

    addNote function linked and addnote.js imported

---
+++ noteApp.js
@@ -0,0 +1,22 @@
+const yargs = require('yargs');
+const noteAdder = require('./addnote.js');
+// Customize yargs
+yargs.command({
+    command: 'add',
+    describe: 'Add a Note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Content of Note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(){
+        noteAdder.addnote(yargs.argv.title,yargs.argv.body);
+    }
+});
