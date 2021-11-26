441eac6550d4c34acaaec2c8d7e56c0e1731be7f
Ankan1998 <ankanroni3@gmail.com>
2021-11-26 13:12:10 +0530

    See a specific note with title

--- getNotes.js
+++ getNotes.js
@@ -9,6 +9,21 @@ const getAllNotes = function(){
     });
 }
 
+const getNote = function(title){
+    console.log(chalk.black.bgGreen.bold("Notes you Searched"));
+    notes = utils.loadnote();
+    const match = notes.filter(function (note) {
+        return utils.similarity(note, title)
+    });
+    if(match.length == 1){
+        console.log(match[0].title);
+        console.log(match[0].body)
+    } else {
+        console.log(chalk.bgRed.bold("Notes not found")); 
+    }
+}
+
 module.exports = {
-    getallnotes: getAllNotes
+    getallnotes: getAllNotes,
+    getnote: getNote
 }
